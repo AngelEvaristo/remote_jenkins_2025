3981f903be0e2506c544d2680b25b29ac1b9563f
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2026-03-12 01:38:38 -0500

    Update and rename test.ps1 to testremote.ps1

--- scripts/test.ps1
+++
@@ -1,6 +0,0 @@
-param(
-  [string]$ProjectName = "Sesion1 .NET Demo"
-)
-Write-Host "== Test: $ProjectName =="
-Write-Host "Simulando tests .NET (ejemplo)"
-# dotnet test
---
+++ scripts/testremote.ps1
@@ -0,0 +1,6 @@
+param(
+  [string]$ProjectName = "Sesion1 .NET Demo"
+)
+Write-Host "== Test: $ProjectName =="
+Write-Host "Simulando tests .NET (ejemplo)"
+# dotnet test
