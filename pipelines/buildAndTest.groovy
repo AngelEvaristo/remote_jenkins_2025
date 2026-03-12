e0ae5afad1515d90421d052b055894a7732a2cce
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2026-03-12 02:06:28 -0500

    Update buildAndTest.groovy

--- pipelines/buildAndTest.groovy
+++ pipelines/buildAndTest.groovy
@@ -1,33 +1,19 @@
-// Remote pipeline step library (loaded via `load`)
-// Usage from Jenkinsfile:
-// def remote = load 'pipelines/buildAndTest.groovy'
-// remote.call('My Project', true)
+def call(String projectName = 'Sesion1 .NET Demo', boolean useProjectScripts = true) {
+  stage('Checkout App') {
+    checkout scm
+  }
 
-def call(String projectName = 'Sesion1 .NET Demo', boolean useProjectScripts = True) {
-  node {
-    stage('Checkout App') {
-      checkout scm
-    }
+  def scriptsDir = useProjectScripts ? 'scripts' : 'pipeline-remote/scripts'
 
-    def scriptsDir = useProjectScripts ? 'scripts' : 'pipeline-remote/scripts'
-
-    stage('Build') {
-      def ws = pwd()  
-      bat "dir"
-      powershell "./${scriptsDir}/buildremote.ps1 -ProjectName '${projectName}'"
-    }
-    stage('Test') {
-      powershell "./${scriptsDir}/testremote.ps1 -ProjectName '${projectName}'"
-    }
-    stage('Notify') {
-      powershell "./${scriptsDir}/notifyremote.ps1 -ProjectName '${projectName}' -Status 'SUCCESS'"
-    }
+  stage('Build') {
+    powershell "./${scriptsDir}/build.ps1 -ProjectName '${projectName}'"
+  }
+  stage('Test') {
+    powershell "./${scriptsDir}/test.ps1 -ProjectName '${projectName}'"
+  }
+  stage('Notify') {
+    powershell "./${scriptsDir}/notify.ps1 -ProjectName '${projectName}' -Status 'SUCCESS'"
   }
 }
 
 return this
-
-
-
-
-
